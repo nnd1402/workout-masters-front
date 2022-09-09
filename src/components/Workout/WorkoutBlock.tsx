feb1373134b0080f42aae613c9c98c2c4e4e490a
nnd1402 <nenadmihajlovic1402@gmail.com>
2022-09-09 15:53:35 +0200

    refactored WorkoutList and WorkoutBlock components

--- src/components/Workout/WorkoutBlock.tsx
+++ src/components/Workout/WorkoutBlock.tsx
@@ -1,80 +1,36 @@
-import { useState } from 'react';
 import { Accordion, Col } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const WorkoutBlock = () => {
-	const [dateInfo, setDateInfo] = useState([
-		{
-			workoutTitle: 'Push Day',
-			workoutDuration: '80 minutes',
-			workoutDescription: 'Pushed for Jesus',
-			workoutDate: new Date()
-		},
-		{
-			workoutTitle: 'Pull Day',
-			workoutDuration: '52 minutes',
-			workoutDescription: 'Pulled all day',
-			workoutDate: new Date('December 9, 1996 03:24:00')
-		},
-		{
-			workoutTitle: 'Leg Day',
-			workoutDuration: '45 minutes',
-			workoutDescription: 'Meh',
-			workoutDate: new Date('December 12, 1997 03:24:00')
-		},
-		{
-			workoutTitle: 'Calisthenics Day',
-			workoutDuration: '90 minutes',
-			workoutDescription: 'wtf',
-			workoutDate: new Date('December 13, 1998 03:24:00')
-		},
-		{
-			workoutTitle: 'Arm Day',
-			workoutDuration: '9 hours',
-			workoutDescription: 'Rich Piana',
-			workoutDate: new Date('December 14, 1999 03:24:00')
-		}
-	]);
-
-	function setStartDate(index: any, date: any) {
-		let updatedDates = [...dateInfo];
-		updatedDates[index].workoutDate = date;
-		setDateInfo(updatedDates);
-		console.log(date);
-	}
-
+const WorkoutBlock = (props: any) => {
 	return (
 		<div>
-			{dateInfo.map((dateInfo, index) => {
-				return (
-					<Accordion>
-						<Accordion.Item eventKey='0'>
-							<Accordion.Header>
-								<Col lg='2' className='header-column '>
-									<h3>{dateInfo.workoutDuration}</h3>
-								</Col>
-								<Col className='header-column text-center'>
-									<h1>{dateInfo.workoutTitle}</h1>
-								</Col>
-								<Col xs lg='2'>
-									<DatePicker
-										custom-attribute={index}
-										className='datepicker text-center'
-										selected={dateInfo.workoutDate}
-										onChange={(date: Date) => setStartDate(index, date)}
-										dateFormat='eee/dd/MM/yyyy'
-									/>
-								</Col>
-							</Accordion.Header>
-							<Accordion.Body>
-								<p>{dateInfo.workoutDescription}</p>
-							</Accordion.Body>
-						</Accordion.Item>
-					</Accordion>
-				);
-			})}
-			;
+			<Accordion>
+				<Accordion.Item eventKey='0'>
+					<Accordion.Header>
+						<Col lg='2' className='header-column '>
+							<h3>{props.workoutDuration}</h3>
+						</Col>
+						<Col className='header-column text-center'>
+							<h1>{props.workoutTitle}</h1>
+						</Col>
+						<Col xs lg='2'>
+							<DatePicker
+								custom-attribute={props.workoutIndex}
+								className='datepicker text-center'
+								selected={props.workoutDate}
+								onChange={(date: Date) =>
+									props.setStartDate(props.workoutIndex, date)
+								}
+								dateFormat='eee/dd/MM/yyyy'
+							/>
+						</Col>
+					</Accordion.Header>
+					<Accordion.Body>
+						<p>{props.workoutDescription}</p>
+					</Accordion.Body>
+				</Accordion.Item>
+			</Accordion>
 		</div>
 	);
 };
--- src/components/Workout/WorkoutList.tsx
+++ src/components/Workout/WorkoutList.tsx
@@ -1,11 +1,69 @@
+import { useState } from 'react';
 import WorkoutBlock from './WorkoutBlock';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { ListGroupItem } from 'react-bootstrap';
 
 const WorkoutList = () => {
+	const [dateInfo, setDateInfo] = useState([
+		{
+			workoutTitle: 'Push Day',
+			workoutDuration: '80 minutes',
+			workoutDescription: 'Pushed for Jesus',
+			workoutDate: new Date()
+		},
+		{
+			workoutTitle: 'Pull Day',
+			workoutDuration: '52 minutes',
+			workoutDescription: 'Pulled all day',
+			workoutDate: new Date('December 9, 1996 03:24:00')
+		},
+		{
+			workoutTitle: 'Leg Day',
+			workoutDuration: '45 minutes',
+			workoutDescription: 'Meh',
+			workoutDate: new Date('December 12, 1997 03:24:00')
+		},
+		{
+			workoutTitle: 'Calisthenics Day',
+			workoutDuration: '90 minutes',
+			workoutDescription: 'wtf',
+			workoutDate: new Date('December 13, 1998 03:24:00')
+		},
+		{
+			workoutTitle: 'Arm Day',
+			workoutDuration: '9 hours',
+			workoutDescription: 'Rich Piana',
+			workoutDate: new Date('December 14, 1999 03:24:00')
+		}
+	]);
+
+	function setStartDate(index: any, date: any) {
+		console.log('bla');
+		console.log(index);
+		let updatedDates = [...dateInfo];
+		updatedDates[index].workoutDate = date;
+		setDateInfo(updatedDates);
+		console.log(date);
+	}
+
 	return (
 		<>
 			<ListGroup>
-				<WorkoutBlock />
+				{dateInfo.map((dateInfo: any, index) => {
+					return (
+						<ListGroupItem>
+							<WorkoutBlock
+								key={index}
+								workoutIndex={index}
+								workoutTitle={dateInfo.workoutTitle}
+								workoutDuration={dateInfo.workoutDuration}
+								workoutDescription={dateInfo.workoutDescription}
+								workoutDate={dateInfo.workoutDate}
+								setStartDate={setStartDate}
+							/>
+						</ListGroupItem>
+					);
+				})}
 			</ListGroup>
 		</>
 	);
