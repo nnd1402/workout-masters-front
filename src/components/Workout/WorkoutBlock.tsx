1cd8d9ec6986bbbb5f79a44a34c3cad065915a93
nnd1402 <nenadmihajlovic1402@gmail.com>
2022-09-12 12:39:15 +0200

    workout title changed to dropdown, datepicker bug fix

--- src/components/Workout/WorkoutBlock.tsx
+++ src/components/Workout/WorkoutBlock.tsx
@@ -1,29 +1,53 @@
+import { useState } from 'react';
 import { Accordion, Col } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const WorkoutBlock = (props: any) => {
+	const workouts = [{ title: 'Push' }, { title: 'Pull' }, { title: 'Leg' }];
+
+	const [workout, setWorkout] = useState();
+
+	const handleWorkoutChange = (e: any) => {
+		setWorkout(e.target.value);
+	};
+
+	function disable(e: any) {
+		e.stopPropagation();
+		e.preventDefault();
+	}
+
 	return (
 		<div>
 			<Accordion>
 				<Accordion.Item eventKey='0'>
-					<Accordion.Header>
+					<Accordion.Header className='accordion-header'>
 						<Col lg='2' className='header-column '>
 							<h3>{props.workoutDuration}</h3>
 						</Col>
 						<Col className='header-column text-center'>
-							<h1>{props.workoutTitle}</h1>
+							<select
+								className='workout-select text-center'
+								onChange={handleWorkoutChange}
+								onClick={disable}
+							>
+								<option>Select a workout</option>
+								{workouts.map((workout) => (
+									<option key={workout.title}>{workout.title}</option>
+								))}
+							</select>
 						</Col>
 						<Col xs lg='2'>
-							<DatePicker
-								custom-attribute={props.workoutIndex}
-								className='datepicker text-center'
-								selected={props.workoutDate}
-								onChange={(date: Date) =>
-									props.setStartDate(props.workoutIndex, date)
-								}
-								dateFormat='eee/dd/MM/yyyy'
-							/>
+							<div onClick={disable}>
+								<DatePicker
+									className='datepicker text-center'
+									selected={props.workoutDate}
+									onChange={(date: Date) =>
+										props.setStartDate(props.workoutIndex, date)
+									}
+									dateFormat='eee/dd/MM/yyyy'
+								/>
+							</div>
 						</Col>
 					</Accordion.Header>
 					<Accordion.Body>
--- src/components/Workout/WorkoutList.tsx
+++ src/components/Workout/WorkoutList.tsx
@@ -6,44 +6,41 @@ import { ListGroupItem } from 'react-bootstrap';
 const WorkoutList = () => {
 	const [dateInfo, setDateInfo] = useState([
 		{
-			workoutTitle: 'Push Day',
+			//workoutTitle: 'Push Day',
 			workoutDuration: '80 minutes',
 			workoutDescription: 'Pushed for Jesus',
 			workoutDate: new Date()
 		},
 		{
-			workoutTitle: 'Pull Day',
+			//workoutTitle: 'Pull Day',
 			workoutDuration: '52 minutes',
 			workoutDescription: 'Pulled all day',
 			workoutDate: new Date('December 9, 1996 03:24:00')
 		},
 		{
-			workoutTitle: 'Leg Day',
+			//workoutTitle: 'Leg Day',
 			workoutDuration: '45 minutes',
 			workoutDescription: 'Meh',
 			workoutDate: new Date('December 12, 1997 03:24:00')
 		},
 		{
-			workoutTitle: 'Calisthenics Day',
+			//workoutTitle: 'Calisthenics Day',
 			workoutDuration: '90 minutes',
 			workoutDescription: 'wtf',
 			workoutDate: new Date('December 13, 1998 03:24:00')
 		},
 		{
-			workoutTitle: 'Arm Day',
+			//workoutTitle: 'Arm Day',
 			workoutDuration: '9 hours',
 			workoutDescription: 'Rich Piana',
 			workoutDate: new Date('December 14, 1999 03:24:00')
 		}
 	]);
 
-	function setStartDate(index: any, date: any) {
-		console.log('bla');
-		console.log(index);
+	function setStartDate(index: any, date: any): void {
 		let updatedDates = [...dateInfo];
 		updatedDates[index].workoutDate = date;
 		setDateInfo(updatedDates);
-		console.log(date);
 	}
 
 	return (
@@ -51,11 +48,11 @@ const WorkoutList = () => {
 			<ListGroup>
 				{dateInfo.map((dateInfo: any, index) => {
 					return (
-						<ListGroupItem>
+						<ListGroupItem key={index}>
 							<WorkoutBlock
 								key={index}
 								workoutIndex={index}
-								workoutTitle={dateInfo.workoutTitle}
+								//workoutTitle={dateInfo.workoutTitle}
 								workoutDuration={dateInfo.workoutDuration}
 								workoutDescription={dateInfo.workoutDescription}
 								workoutDate={dateInfo.workoutDate}
