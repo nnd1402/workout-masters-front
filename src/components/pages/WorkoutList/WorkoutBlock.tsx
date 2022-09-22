4f50dcba48113e9d527d35784dba573ac3354186
Nenad <nenadmihajlovic1402@gmail.com>
2022-09-22 20:21:55 +0200

    switched list from standard accordion to collapse,
    Switching fixed the warning for DatePicker, added some css

--- src/components/pages/WorkoutList/WorkoutBlock.tsx
+++ src/components/pages/WorkoutList/WorkoutBlock.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { Accordion, Row, Col } from 'react-bootstrap';
+import { Accordion, Row, Col, Card, Button, Collapse } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import WorkoutDeleteBtn from './components/WorkoutDeleteBtn';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 const WorkoutBlock = (props: any) => {
 	const workouts = [{ title: 'Push' }, { title: 'Pull' }, { title: 'Leg' }];
@@ -19,40 +21,54 @@ const WorkoutBlock = (props: any) => {
 		e.stopPropagation();
 	}
 
+	const [open, setOpen] = useState(false);
+
 	return (
 		<>
-			<Accordion>
-				<Accordion.Item eventKey='0'>
-					<Accordion.Header className='accordion-header'>
-						<Col className='header-column '>
-							<h3>{props.workoutDuration} minutes</h3>
-						</Col>
-						<Col className='header-column text-center'>
-							<select
-								className='workout-select text-center'
-								onChange={handleWorkoutChange}
-								onClick={disablePropagation}
-							>
-								<option>{workout}</option>
-								{workouts.map((workout) => (
-									<option key={workout.title}>{workout.title}</option>
-								))}
-							</select>
-						</Col>
-						<Col>
-							<div className='text-center' onClick={disablePropagation}>
-								<DatePicker
-									className='datepicker text-center'
-									selected={startDate}
-									onChange={(date: Date) => setStartDate(date)}
-									showTimeSelect
-									dateFormat='eee/dd/MM/yyyy HH:mm:ss'
-								/>
-							</div>
-						</Col>
-					</Accordion.Header>
-
-					<Accordion.Body>
+			<Accordion defaultActiveKey='0'>
+				<Card>
+					<Card.Header className='card-header'>
+						<Row>
+							<Col md={3} className='header-column'>
+								<h3 className='h3-duration'>{props.workoutDuration} minutes</h3>
+							</Col>
+							<Col md={4} className='header-column text-center'>
+								<select
+									className='workout-select text-center'
+									onChange={handleWorkoutChange}
+									onClick={disablePropagation}
+								>
+									<option>{workout}</option>
+									{workouts.map((workout) => (
+										<option key={workout.title}>{workout.title}</option>
+									))}
+								</select>
+							</Col>
+							<Col md={4}>
+								<div className='text-center' onClick={disablePropagation}>
+									<DatePicker
+										className='datepicker text-center'
+										selected={startDate}
+										onChange={(date: Date) => setStartDate(date)}
+										showTimeSelect
+										dateFormat='eee/dd/MM/yyyy HH:mm:ss'
+									/>
+								</div>
+							</Col>
+							<Col md={1} className='text-center'>
+								<Button
+									className={`${
+										open ? 'accordion-button' : 'accordion-button collapsed'
+									}`}
+									variant='transparent'
+									onClick={() => setOpen(!open)}
+									aria-controls='example-collapse-text'
+									aria-expanded={open}
+								></Button>
+							</Col>
+						</Row>
+					</Card.Header>
+					<Collapse className='collapse-body' in={open}>
 						<Row>
 							<Col md={11}>
 								<p>{props.workoutDescription}</p>
@@ -65,8 +81,8 @@ const WorkoutBlock = (props: any) => {
 								/>
 							</Col>
 						</Row>
-					</Accordion.Body>
-				</Accordion.Item>
+					</Collapse>
+				</Card>
 			</Accordion>
 		</>
 	);
